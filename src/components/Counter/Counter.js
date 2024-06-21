2b87f98b8cc49558872c1a3bd0c4eb284c7db247
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-06-21 13:31:39 -0500

    add useEffect to encapsulate the server side code being used for saving values

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,12 +1,17 @@
 'use client';
 import React from 'react';
 
+import Spinner from '../Spinner';
+
 function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
-  });
+  const [count, setCount] = React.useState(null);
+
+  // so useEffect does not get rendered on the server, so it is only running one time
+  // from the client
+  React.useEffect(() => {
+    const savedValue = window.localStorage.getItem('saved-count');
+    setCount(savedValue ? Number(savedValue) : 0)
+  }, [])
 
   React.useEffect(() => {
     window.localStorage.setItem('saved-count', count);
@@ -17,7 +22,8 @@ function Counter() {
       className="count-btn"
       onClick={() => setCount(count + 1)}
     >
-      Count: {count}
+      Count:{' '}
+      {typeof count === 'number' ? count : <Spinner />}
     </button>
   );
 }
